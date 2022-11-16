feccf76041ed0c2950b4f366b67f88f60e58567e
Your Name <you@example.com>
2022-11-16 20:20:31 +0100

    Created Vue+Vite+TypesCript app with npx create vue app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Welcome from "./components/Welcome.vue";
+</script>
+
+<template>
+  <Welcome msg="MOBILES APP" />
+</template>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2 class="heading__section">Tables</h2>
+</template>
+
+<style scoped>
+.heading__section {
+  color: #888;
+}
+</style>
