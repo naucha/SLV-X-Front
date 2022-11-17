f1ccca6b516c5f1264d84291a98baa944a79d3b1
Your Name <you@example.com>
2022-11-17 22:32:30 +0100

    Create mobile item component

---
+++ src/components/Mobile.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { defineProps } from "vue";
+type Props = {
+  brand: string;
+  model: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <tr>
+      <th scope="row">{{ props.brand }}</th>
+      <th scope="row">{{ props.model }}</th>
+      <th scope="row">{{}}</th>
+    </tr>
+  </div>
+</template>
+
+<style lang="css" scoped>
+th {
+  padding: 6px;
+  width: 110px;
+  height: 30px;
+}
+</style>
