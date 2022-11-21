68198ed7752151b7a465e94f20da76a9f7976739
Your Name <you@example.com>
2022-11-21 08:40:36 +0100

    Create new apiService, addMobile

--- src/api/mobileService.ts
+++ src/api/mobileService.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 import { IMobile } from "../types/types";
 
+const url = `${import.meta.env.VITE_API_URL}`;
+
 export const getMobiles = async (): Promise<IMobile[]> => {
-  let url = `${import.meta.env.VITE_API_URL}`;
   return await axios.get(`${url}api/mobiles`).then((response) => {
     return response.data;
   });
+};
+
+export const addMobile = async (mobile: any): Promise<void> => {
+  try {
+    const { data } = await axios.post(`${url}api/mobiles`, mobile);
+  } catch (error) {
+    console.log("not added");
+  }
 };
