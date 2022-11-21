58bc7284d021efee5855afabfc0f338040009ed9
Your Name <you@example.com>
2022-11-21 08:44:27 +0100

    Create formulary for add/create new entries by accesssing to store

---
+++ src/components/FormAddMobile.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { mobileStore } from "../store/mobilesStore";
+import { IMobileRequest } from "../types/types";
+
+const newMobile = ref<IMobileRequest>({ model: "", brand: "" });
+
+const saveNewMobile = () => {
+  const mobile = {
+    model: newMobile.value.model,
+    brand: newMobile.value.brand,
+  };
+
+  mobileStore.add(mobile);
+  newMobile.value = { model: "", brand: "" };
+};
+</script>
+
+<template>
+  <form @submit.prevent="saveNewMobile">
+    <input type="text" placeholder="Insert brand" v-model="newMobile.brand" />
+    <input type="text" placeholder="Insert model" v-model="newMobile.model" />
+    <button type="submit">✚ ADD MOBILE</button>
+  </form>
+</template>
+
+<style lang="css" scoped>
+form {
+  width: 250px;
+}
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
