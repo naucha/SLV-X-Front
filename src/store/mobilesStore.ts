b992bb223f6450c03d4afa1bef61bb75bfec4ad9
Your Name <you@example.com>
2022-11-24 16:26:08 +0100

    Create update action in store. Add a boolean which allow us to show updateform

--- src/store/mobilesStore.ts
+++ src/store/mobilesStore.ts
@@ -1,17 +1,20 @@
 import { reactive } from "vue";
-import { deleteOne, getMobiles } from "../api/mobileService";
-import { IMobile, IMobileRequest } from "../types/types";
+import { deleteOne, getMobiles, updateMobile } from "../api/mobileService";
+import { IMobile, IMobileRequest, IMobileUpdate } from "../types/types";
 import { addMobile } from "../api/mobileService";
 
 type MobileStore = {
   mobileList: IMobile[];
+  showUpdate: boolean;
   get: () => Promise<void>;
   add: (mobile: IMobileRequest) => Promise<void>;
   delete: (id: number) => Promise<void>;
+  update: (mobile: IMobileUpdate) => Promise<void>;
 };
 
 export const mobileStore: MobileStore = reactive({
   mobileList: [],
+  showUpdate: false,
   get: async () => {
     const apiMobiles = await getMobiles();
     mobileStore.mobileList = apiMobiles;
@@ -22,7 +25,10 @@ export const mobileStore: MobileStore = reactive({
   },
   delete: async (id: number) => {
     deleteOne(id);
-
     mobileStore.get();
+  },
+  update: async (mobile) => {
+    updateMobile(mobile);
+    await mobileStore.get();
   },
 });
