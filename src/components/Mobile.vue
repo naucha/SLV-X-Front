edbd7a2d9bb8dceb0edbee4b2ee68604bf198220
Your Name <you@example.com>
2022-12-09 16:19:28 +0100

    Get the computed rates in a Store and add a conditional rendering in Mobile. If it recieves a rate then it shows a third row with n rates

--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+import { mobileStore } from "../store/mobilesStore";
+
 type Props = {
   brand: string;
   model: string;
+  rate?: number;
+  id: number;
 };
 
 const props = defineProps<Props>();
+const rates: any = computed(() => mobileStore.ratesList);
 </script>
 
 <template>
-  <div>
+  <div v-if="props.rate !== undefined">
     <tr>
       <th scope="row">{{ props.brand }}</th>
       <th scope="row">{{ props.model }}</th>
-      <th scope="row">{{}}</th>
+      <th scope="row">Nº Rates: {{ rates[props.rate] }}</th>
+    </tr>
+  </div>
+
+  <div v-else>
+    <tr>
+      <th scope="row">{{ props.brand }}</th>
+      <th scope="row">{{ props.model }}</th>
     </tr>
   </div>
 </template>
