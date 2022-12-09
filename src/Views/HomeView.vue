38a87b0eb96658679fdf4524662f8ebb1d0ba0c7
Your Name <you@example.com>
2022-12-09 16:48:20 +0100

    Instance of TableRating in home view

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -1,11 +1,21 @@
+<script lang="ts" setup>
+import MobilesList from "../components/MobilesList.vue";
+import FormAddMobile from "../components/FormAddMobile.vue";
+import TableRating from "../components/TableRating.vue";
+</script>
+
 <template>
   <div>
     <MobilesList />
     <FormAddMobile />
+    <TableRating />
   </div>
 </template>
 
-<script lang="ts" setup>
-import MobilesList from "../components/MobilesList.vue";
-import FormAddMobile from "../components/FormAddMobile.vue";
-</script>
+<style lang="css" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
