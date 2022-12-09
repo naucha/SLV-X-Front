c15ff4dae0171b29390b5ae438e60ef40599faa9
Your Name <you@example.com>
2022-12-09 16:10:21 +0100

    Create new Table to show number of rates for every Mobile

---
+++ src/components/TableRating.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+
+import { mobileStore } from "../store/mobilesStore";
+import Mobile from "./Mobile.vue";
+
+const mobiles = computed(() => mobileStore.mobileList);
+</script>
+
+<template>
+  <h2>Table with number of ratings</h2>
+  <table>
+    <tr>
+      <th scope="col">Brand</th>
+      <th scope="col">Model</th>
+      <th scope="col">Rates</th>
+    </tr>
+  </table>
+  <div v-for="(mobile, index) in mobiles">
+    <Mobile
+      :brand="mobile.brand"
+      :model="mobile.model"
+      :id="mobile.id"
+      :rate="index"
+      :key="mobile.id"
+    />
+  </div>
+</template>
+
+<style lang="css" scoped>
+table {
+  margin-top: 10px;
+  width: 400px;
+  height: 40px;
+  border: 1px solid white;
+}
+</style>
