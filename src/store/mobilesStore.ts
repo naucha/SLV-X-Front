a8f69f5a03576e4912650e2a11897e315425a167
Your Name <you@example.com>
2022-12-09 16:07:04 +0100

    Create getCountByid action, save in ratesLsit array. Add await in other actions

--- src/store/mobilesStore.ts
+++ src/store/mobilesStore.ts
@@ -1,34 +1,52 @@
 import { reactive } from "vue";
-import { deleteOne, getMobiles, updateMobile } from "../api/mobileService";
-import { IMobile, IMobileRequest, IMobileUpdate } from "../types/types";
+import {
+  deleteOne,
+  getMobiles,
+  getMobileRatingsCount,
+  updateMobile,
+} from "../api/mobileService";
+import { IMobileRequest, IMobileUpdate } from "../types/types";
 import { addMobile } from "../api/mobileService";
 
 type MobileStore = {
-  mobileList: IMobile[];
+  mobileList: any[];
+  ratesList: any[];
   showUpdate: boolean;
   get: () => Promise<void>;
   add: (mobile: IMobileRequest) => Promise<void>;
   delete: (id: number) => Promise<void>;
   update: (mobile: IMobileUpdate) => Promise<void>;
+  getCountById: (id: number) => Promise<void>;
 };
 
 export const mobileStore: MobileStore = reactive({
   mobileList: [],
+  ratesList: [],
   showUpdate: false,
+
   get: async () => {
     const apiMobiles = await getMobiles();
     mobileStore.mobileList = apiMobiles;
+
+    let testMobile: any = await apiMobiles.map((mobile) =>
+      mobileStore.getCountById(mobile.id)
+    );
+    mobileStore.ratesList = await Promise.all(testMobile);
   },
   add: async (mobile: IMobileRequest) => {
-    addMobile(mobile);
-    mobileStore.get();
+    await addMobile(mobile);
+    await mobileStore.get();
   },
   delete: async (id: number) => {
-    deleteOne(id);
-    mobileStore.get();
+    await deleteOne(id);
+    await mobileStore.get();
   },
   update: async (mobile) => {
-    updateMobile(mobile);
+    await updateMobile(mobile);
     await mobileStore.get();
+  },
+  getCountById: async (id: number) => {
+    const apiRatingCount = await getMobileRatingsCount(id);
+    return apiRatingCount[0].length;
   },
 });
